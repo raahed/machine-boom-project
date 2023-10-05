cd3356478d6e2a163c31842c1809079623aa9d20
David Schwenke <schwenkedavid@t-online.de>
2023-10-05 14:05:54 +0200

    Joint refactor

---
+++ src/simulation/include/joints.hpp
@@ -0,0 +1,71 @@
+#ifndef JOINTS_HPP
+#define JOINTS_HPP
+
+#include <string>
+#include <vector>
+#include <tuple>
+#include <agx/Constraint.h>
+#include "constraints.hpp"
+
+class Joint
+{
+public:
+    virtual void setVelocities(const std::vector<double> &newVelocities) = 0;
+    virtual const std::vector<double> getVelocities() = 0;
+    virtual const std::vector<double> getAngles() = 0;
+    virtual const std::string getName() = 0;
+};
+
+template <class T>
+class Joint1DOF : Joint, Convertible_to<T, agx::Constraint1DOF>
+{
+    std::string name;
+    T constraint;
+
+public:
+    Joint1DOF(std::string _name, T _constraint) : name(_name), constraint(_constraint){};
+
+    ~Joint1DOF()
+    {
+        delete name;
+        delete constraint;
+    };
+
+    void setVelocities(const std::vector<double> &newVelocities);
+    const std::vector<double> getVelocities();
+    const std::vector<double> getAngles();
+    const std::string getName();
+};
+
+template <class T>
+class Joint2DOF : Joint, Convertible_to<T, agx::Constraint2DOF>
+{
+    std::string name;
+    T constraint;
+
+    void setVelocity(agx::Constraint2DOF::DOF dof, const double &newVelocity);
+    const double getVelocity(agx::Constraint2DOF::DOF dof);
+    const double getAngle(agx::Constraint2DOF::DOF dof);
+
+public:
+    Joint2DOF(std::string _name, T _constraint) : name(_name), constraint(_constraint) 
+    {
+        constraint.getMotor1D(agx::Constraint2DOF::DOF::FIRST)->setEnable(true);
+        constraint.getMotor1D(agx::Constraint2DOF::DOF::FIRST)->setLocked(true);
+        constraint.getMotor1D(agx::Constraint2DOF::DOF::SECOND)->setEnable(true);
+        constraint.getMotor1D(agx::Constraint2DOF::DOF::SECOND)->setLocked(true);
+    };
+
+    ~Joint2DOF()
+    {
+        delete name;
+        delete constraint;
+    };
+
+    void setVelocities(const std::vector<double> &newVelocities);
+    const std::vector<double> getVelocities();
+    const std::vector<double> getAngles();
+    const std::string getName();
+};
+
+#endif
---
+++ src/simulation/src/Joint1DOF.cpp
@@ -0,0 +1,32 @@
+#include "joints.hpp"
+
+template <class T>
+void Joint1DOF<T>::setVelocities(const std::vector<double> &newVelocities)
+{
+    if (newVelocities.size() != 1)
+    {
+        throw std::invalid_argument("For one degree of freedom pass one velocity!");
+    }
+    constraint.getMotor1D()->setEnable(true);
+    constraint.getMotor1D()->setLocked(false);
+    constraint.getMotor1D()->setSpeed(newVelocities[0]);
+};
+
+template <class T>
+const std::vector<double> Joint1DOF<T>::getVelocities()
+{
+    return constraint.getMotor1D()->getSpeed();
+};
+
+template <class T>
+const std::vector<double> Joint1DOF<T>::getAngles()
+{
+    std::vector<double> angles{constraint.getAngle()};
+    return angles;
+};
+
+template <class T>
+const std::string Joint1DOF<T>::getName()
+{
+    return name;
+};
