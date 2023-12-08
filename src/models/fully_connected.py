3e976312d8f4e672cdc2bbb5b058afe573e943bb
Marvin Fette <fettemarvin@gmail.com>
2023-12-08 17:05:06 +0100

    Prepare FFN

--- src/models/fully_connected.py
+++ src/models/fully_connected.py
@@ -9,34 +9,11 @@ from pathlib import Path
 from ray import train as ray_train
 from typing import List
 
-feature_columns = [
-    'left_boom_base_yaw_joint', 
-    'left_boom_base_pitch_joint',
-    'left_boom_main_prismatic_joint',
-    'left_boom_second_roll_joint',
-    'left_boom_second_yaw_joint',
-    'left_boom_top_pitch_joint'
-]
-
-label_columns = [
-    'cable1_lowest_point',
-    'cable2_lowest_point',
-    'cable3_lowest_point'
-]
-
 class FullyConnected(nn.Module):
     def __init__(self, flattened_input_dim: int, intermediate_dims: List[int],
-                 output_dim: int, dropout: float = 0.25, hidden_activation: str = 'relu') -> None:
+                 output_dim: int, dropout: float = 0.25, hidden_activation: nn.Module = nn.ReLU) -> None:
         super().__init__()
         self.total_epochs = 0
-
-        if hidden_activation == 'relu':
-            hidden_activation_class = nn.ReLU
-        elif hidden_activation == 'tanh':
-            hidden_activation_class = nn.Tanh
-        else:
-            raise ValueError("Can not interfer hidden activation")
-
         self.flatten = nn.Flatten()
         self.hidden = nn.Sequential()
         for i, dim in enumerate(intermediate_dims):
@@ -44,7 +21,7 @@ class FullyConnected(nn.Module):
                 self.hidden.add_module(f"linear_{i+1}", nn.Linear(flattened_input_dim, dim))
             else:
                 self.hidden.add_module(f"linear_{i+1}", nn.Linear(intermediate_dims[i-1], dim))
-            self.hidden.add_module(f"hidden_activation_{i+1}", hidden_activation_class())
+            self.hidden.add_module(f"hidden_activation_{i+1}", hidden_activation())
             self.hidden.add_module(f"dropout_{i+2}", nn.Dropout(dropout))
 
         self.last = nn.Linear(intermediate_dims[-1], output_dim)
@@ -54,14 +31,6 @@ class FullyConnected(nn.Module):
         x = self.hidden(x)
         return self.last(x)
     
-
-def get_optimizer_function(model: nn.Module, learning_rate: float) -> torch.optim:
-    return torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-
-def get_loss_function() -> nn.Module:
-    return torch.nn.MSELoss()
-
 
 def train_epoch(train_dataloader: DataLoader, model: nn.Module, loss_function, optimizer, 
                 device: torch.device, report_interval: int = 1000) -> float:
--- src/utils/loss_functions.py
+++ src/utils/loss_functions.py
@@ -2,3 +2,7 @@ import torch
 
 def maximum_squared_error(output, target):
     return torch.sum(torch.max((output - target)** 2, dim=0)[0])
+
+
+def get_loss_function():
+    return torch.nn.MSELoss()
